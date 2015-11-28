af67b4d0412e368ab8518a774ce0e6a2a9e8382c
leonelgpr33 <leonel.perez@mrci.com.mx>
2015-11-28 00:25:48 -0600

    Commit Mensaje de Bienvenida

---
+++ Phone Register/Register.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Phone Register
+//
+//  Created by LI Leonel G. Pérez Ramos on 27/11/15.
+//  Copyright © 2015 LI Leonel G. Pérez Ramos. All rights reserved.
+//
+
+#import "Register.h"
+
+@interface Register ()
+
+@end
+
+@implementation Register
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
